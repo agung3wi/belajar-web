8f903a5afe2200d58f1a498471bde8b41c004bdd
Agung <agungskripsi05@gmail.com>
2022-12-29 16:23:13 +0700

    vue

--- laravel/app/Http/Controllers/CustomerController.php
+++ laravel/app/Http/Controllers/CustomerController.php
@@ -10,9 +10,9 @@ class CustomerController extends Controller
 {
     public function index()
     {
-        
+
         $customers = Customer::all(); // select * from customers
-        return view('customer.index', ["customers" => $customers]);    
+        return view('customer.index', ["customers" => $customers]);
     }
 
     public function add()
@@ -22,37 +22,57 @@ class CustomerController extends Controller
 
     public function edit($id)
     {
-        $customer = Customer::find($id); // select * from where id = ? 
-        
-        return view('customer.edit',["customer" => $customer]);
+        $customer = Customer::find($id); // select * from where id = ?
+
+        return view('customer.edit', ["customer" => $customer]);
     }
 
     public function update($id)
     {
-        $customer = Customer::find($id); // select * from where id = ? 
+        $customer = Customer::find($id); // select * from where id = ?
         $customer->name = request("name", "");
         $customer->phone = request("phone", "");
         $customer->address = request("address", "");
         $customer->save();
-     
+
         return redirect('customer');
     }
 
     public function delete($id)
     {
-        $customer = Customer::find($id); // select * from where id = ? 
+        $customer = Customer::find($id); // select * from where id = ?
         $customer->delete();
         return redirect('customer');
     }
 
     public function store()
     {
+        // validasi data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'address' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json($errors, 422);
+        }
+
+        // validasi business
+        $cityExists = City::find(request("city_id", -1));
+        if(is_null($cityExists))
+        {
+            return response()->json([
+                "messsage" => "Kota dengan id ... tidak ditemukan"
+            ], 422);
+        }
+
         $customer = new Customer();
         $customer->name = request("name", "");
         $customer->phone = request("phone", "");
+        $customer->city_id = request("city_id", -1);
         $customer->address = request("address", "");
         $customer->save();
         return redirect('customer');
     }
-    
 }
---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div>
+      <b-table striped hover :items="items"></b-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      items: [
+      ]
+    }
+  },
+  mounted () {
+    axios
+      .get('https://laravel-api.zzidzz.tech/api/product')
+      .then(response => (
+        this.items = response.data
+      ))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
